d71efa1a2019d4a3b7c9555ccf75f284437ea8f7
Gilberto Pérez <betho6990@gmail.com>
2026-07-17 12:00:35 -0600

    Implementing Pending Reply Warehouse

---
+++ products-command/src/main/java/com/springcloud/kafka/products_command/messaging/ReplyInbox.java
@@ -0,0 +1,30 @@
+package com.springcloud.kafka.products_command.messaging;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+import com.springcloud.kafka.products_command.models.Repply;
+
+@Component
+public class ReplyInbox {
+    private final ConcurrentHashMap<String, CompletableFuture<Repply<?>>> pending = new ConcurrentHashMap<>();
+
+    public CompletableFuture<Repply<?>> register(String correlationId) {
+        CompletableFuture<Repply<?>> future = new CompletableFuture<>();
+        return future;
+     }
+
+     public void complete(String correlationId, Repply<?> repply) {
+        // CompletableFuture<Repply<?>> future = new CompletableFuture<>();
+        if (correlationId == null) {
+            throw new NullPointerException("correlationId can't be null");
+        }
+        CompletableFuture<Repply<?>> future = pending.remove(correlationId);
+        if (future != null) {
+            
+            future.complete(repply);
+        }
+     }
+}
